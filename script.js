d4561fe20263fb4bb058b95ef1cdfc82b8b45682
Hasoni7 <Hasan.oda@outlook.com>
2025-04-21 20:10:42 +0200

    Created typing game
    Created a simple typing game

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
 const settingsBtn = document.getElementById("settings-btn");
 const difficultySelect = document.getElementById("difficulty");
+const timeInterval = setInterval(updateTime, 1000);
 
 // Array
 const words = [
@@ -40,3 +41,57 @@ let score = 0;
 
 //Initializing time
 let time = 10;
+
+//functions
+function addWordToDOM() {
+  randomWord = words[Math.floor(Math.random() * words.length)];
+  word.innerText = randomWord;
+}
+
+function updateScore() {
+  score++;
+  scoreEl.innerText = score;
+}
+
+function updateTime() {
+  time--;
+  updateTimeDisplay();
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+function updateTimeDisplay() {
+  timeEl.innerText = `${time}s`;
+}
+
+function gameOver() {
+  endgameEl.innerText = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+  endgameEl.style.display = "flex";
+}
+
+// EVENT LISTENERS
+
+text.addEventListener("input", () => {
+  const insertedText = text.value.trim();
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+    time += 5;
+    updateTimeDisplay();
+    text.value = "";
+  }
+});
+
+//optional part
+settingsBtn.addEventListener("click", () => {
+  settings.classList.toggle("hide");
+});
+
+addWordToDOM();
